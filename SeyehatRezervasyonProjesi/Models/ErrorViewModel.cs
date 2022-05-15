e46a4d6bc0ba494bfc9a6a39be0a7232d4b6bb7e
= <=>
2022-05-15 22:42:26 +0300

    Proje dosyası ekle.

---
+++ SeyehatRezervasyonProjesi/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SeyehatRezervasyonProjesi.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
