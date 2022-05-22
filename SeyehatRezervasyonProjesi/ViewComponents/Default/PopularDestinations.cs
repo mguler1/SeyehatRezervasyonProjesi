4d7c4be0573f4e84af98f2ca7f43af3384aea797
= <=>
2022-05-22 20:43:18 +0300

    Index düzenlemeri devam edecek

---
+++ SeyehatRezervasyonProjesi/ViewComponents/Default/PopularDestinations.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SeyehatRezervasyonProjesi.ViewComponents.Default
+{
+    public class PopularDestinations:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ SeyehatRezervasyonProjesi/ViewComponents/Default/Slider.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SeyehatRezervasyonProjesi.ViewComponents.Default
+{
+    public class Slider:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
