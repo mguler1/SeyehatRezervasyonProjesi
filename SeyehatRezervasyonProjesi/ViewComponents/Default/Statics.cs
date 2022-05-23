cbcd22997e7e5f77064d9a1ba7d1726bf39c0b8c
= <=>
2022-05-23 23:44:05 +0300

    İstatistikler dinamik hale getirildi

---
+++ SeyehatRezervasyonProjesi/ViewComponents/Default/Statics.cs
@@ -0,0 +1,18 @@
+﻿using DataAccess.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace SeyehatRezervasyonProjesi.ViewComponents.Default
+{
+    public class Statics:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            using var context=new Context();
+            ViewBag.Rotalar = context.Destinations.Count();
+            ViewBag.Rehberler = context.Guides.Count();
+
+            return View();
+        }
+    }
+}
