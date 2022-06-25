fc993d1893fa6416da68bd5243a6e43f1d7aa6ba
= <=>
2022-06-26 00:47:44 +0300

    Rezervasyon ekleme işlemleri devam ediyor

---
+++ Business/Concrete/ReservationManager.cs
@@ -0,0 +1,49 @@
+﻿using Business.Abstarct;
+using Core.Result.Abstarct;
+using Core.Result.Concrete;
+using DataAccess.Abstract;
+using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class ReservationManager : IReservationService
+    {
+        IReservationDal _reservationDal;
+
+        public ReservationManager(IReservationDal reservationDal)
+        {
+            _reservationDal = reservationDal;
+        }
+
+        public IResult Add(Reservation entity)
+        {
+            _reservationDal.Insert(entity);
+            return new SuccessResult();
+        }
+
+        public IResult Delete(Reservation entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Reservation GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Reservation> GetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult Update(Reservation entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- SeyehatRezervasyonProjesi/Areas/Member/Controllers/ReservationController.cs
+++ SeyehatRezervasyonProjesi/Areas/Member/Controllers/ReservationController.cs
@@ -1,11 +1,18 @@
-﻿using Entity.Concrete;
+﻿using Business.Concrete;
+using DataAccess.Ef;
+using Entity.Concrete;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SeyehatRezervasyonProjesi.Areas.Member.Controllers
 {
     [Area("Member")]
     public class ReservationController : Controller
     {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
+        ReservationManager reservationManager = new ReservationManager(new EfReservationDal());
         [HttpGet]
         public IActionResult MyCurrentReservation()
         {
@@ -19,12 +26,21 @@ namespace SeyehatRezervasyonProjesi.Areas.Member.Controllers
         [HttpGet]
         public IActionResult NewReservation()
         {
+            List<SelectListItem> values = (from x in destinationManager.GetList()
+                                           select new SelectListItem
+                                           {
+                                               Text=x.City  ,
+                                               Value=x.DestinationId.ToString()
+                                           }).ToList();
+            ViewBag.Values = values;
             return View();
         }
         [HttpPost]
         public IActionResult NewReservation(Reservation model)
         {
-            return View();
+            model.AppUserId = 3;
+            reservationManager.Add(model);
+            return RedirectToAction("MyCurrentReservation");
         }
     }
 }
